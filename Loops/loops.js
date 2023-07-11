e00b2877d61768e465e5b11c8ecb52bf129983fe
Hameedullah Asadi <hameedasade20@gmail.com>
2023-07-11 15:09:10 -0700

    math
    worked on marh.random and ParseInt

--- Loops/loops.js
+++ Loops/loops.js
@@ -10,7 +10,6 @@ To do whule loop, we need 3 things:
 
 //  While Loop - loops through a block of code as long as speicified condition is true
 
-
 //  While loop iterates string data type
 var i = 1;  //  This is intialization
 while (i <= 10)     //  This is condition that we set
@@ -63,7 +62,6 @@ for (var i = 1; i <= 5; i++)    //  All 3 initialization, condition and incremen
     console.log("Hey, I am a web developer!");
 }
 
-
 /*  Nested For Loop  - This neted for loop will print one teim "Hi, and then five times
 "I am software developer!". again one time "Hi", and then five times "I am a software developer"
 and goes one.
@@ -74,4 +72,30 @@ for (var i = 1; i <= 5; i++)        //This is the first loop
     for (var j = 1; j <= 5; j++)    //This is the second loop
         console.log("I am a software developer!");  
 }
+
+    //  For Loop and incremention by 2
+    var myArray = []
+for (var i = 1; i <= 10; i += 2)    //  This is wil increment the value by 2. We can choose any number we want
+    myArray.push(i);        //  We are adding the value into the array
+{
+    console.log(myArray); //  The output is 1, 3, 5, 7, and 9
+}
+
+//  Loop through an array and add the values
+var loopArray = [15, 25, 35, 45, 55, 65, 75, 85, 95,]
+var total = 0;
+for (var i = 0; i < loopArray.length; i++)
+{
+    total += loopArray[i];
+}
+    console.log(total)
+
+
+
+
+
+
+
+
+
 
