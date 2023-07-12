820ae9dbf12fb551a46ecdbfa1ce9c99ceb7c75e
Hameedullah Asadi <hameedasade20@gmail.com>
2023-07-12 16:36:31 -0700

    function

--- Functions/functions.js
+++ Functions/functions.js
@@ -191,25 +191,3 @@ console.log(totalSum);  //  prints 600
 
 /*------------------------*/
 
-
-/*  Template Literal - These are templates delimited with backtick characters, allowing for multiline srings
-It is also called:
-Template string
-String template
-Backtick syntax
-*/
-let templateLiteral =   //  Each prints on a new line
-    `I love coding in following programming language:          
-JavaScript
-and CSharp.`;
-console.log(templateLiteral);
-
-
-/*------------------------*/
-
-
-//Template literal also allows variables in string as follows:
-let firstName = "Hameedullah";
-let lastName = "Asadi";
-let result = `My full name is ${firstName} ${lastName}.`;   //We placed variabled inside the template string so whenever the value is changed, the string is also changed automatically
-console.log(result);        //  We stored all the values inside the variable named "result"
