40bb143e3b38cbbd92b0d66940968ab05850fbdc
Hameedullah Asadi <hameedasade20@gmail.com>
2023-07-16 14:55:32 -0700

    worked on Number properties

---
+++ Numbers/number_properties.js
@@ -0,0 +1,44 @@
+/*
+Number properties can not be used on variables, it belongs to JavaScript Number object"
+*/
+
+//Number.Epsilon - is the difference between the smallest floating point number greater than 1 and 1
+console.log(Number.EPSILON);    //  Returns 2.220446049250313e-16
+
+/*--------------------*/
+
+//  Number.MAX_VALUE    -   It returns the largest possible number in JavaScript
+console.log(Number.MAX_VALUE);      //  Returns 1.7976931348623157e+308
+
+/*--------------------*/
+
+//  Number.MIN_VALUE    -   It returns the lowest possible number
+console.log(Number.MIN_VALUE);  //  Returns 5e-324
+
+/*--------------------*/
+
+//  Number.MAX_SAFE_INTEGER     -       It is used to return the maximum safest integer in JavaScript
+console.log(Number.MAX_SAFE_INTEGER);   //  Returns (253 - 1)
+
+/*--------------------*/
+
+//  Number.MIN_SAFE_INTEGER     -       It is used to return the minimum safest integer in JavaScript
+console.log(Number.MIN_SAFE_INTEGER);   //Returns -(253 - 1)
+
+/*--------------------*/
+
+//  Number.POSITIVE_INFINITY    -       
+console.log(Number.POSITIVE_INFINITY);  //  Returns Infinity
+
+/*--------------------*/
+
+console.log(Number.NEGATIVE_INFINITY);  //  Returns -Infinity
+
+/*--------------------*/
+
+//NaN   -   It is JavaScript keyword used for a number that is not a legal number
+let num2 = 10 / "JavaScript"
+console.log(num2);      //  Returns NaN
+
+/*--------------------*/
+
