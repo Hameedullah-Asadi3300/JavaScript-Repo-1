4e8b11289e773a0f7c22e9cad1c728f9455813cd
Hameedullah Asadi <hameedasade20@gmail.com>
2023-07-17 20:16:33 -0700

    added a few other date methods

--- Date/date_method.js
+++ Date/date_method.js
@@ -20,10 +20,23 @@ const method4 = new Date();
 method4.toLocaleString();   //  Returns '7/17/2023, 7:44:32 PM'
 
 /*--------------------*/
-//  toJSON  -   Returns the date as a string, formatted as a JSON date
+//  toJSON()  -   Returns the date as a string, formatted as a JSON date
 const method5 = new Date();
 method5.toJSON();   //  Returns '2023-07-18T02:45:49.383Z'
 
+
+/*
+JavaScript get Methods are follows:
+Method	                            Description
+getFullYear()	            Get year as a four digit number (yyyy)
+getMonth()	                Get month as a number (0-11)
+getDate()	                Get day as a number (1-31)
+getDay()	                Get weekday as a number (0-6)
+getHours()	                Get hour (0-23)
+getMinutes()	            Get minute (0-59)
+getSeconds()	            Get second (0-59)
+
+*/
 /*--------------------*/
 //  getFullYear()   -   Returns the full current year
 const method6 = new Date();
@@ -39,6 +52,22 @@ method7.getDay();   //  Returns number from 0-6
 const method8 = new Date();
 method8.getDate();  //  Returns number like 17
 
+/*--------------------*/
+//  GetHours
+const method9 = new Date();
+method9.getHours();     //  Retuns hours from 0-23
+
+/*--------------------*/
+//  getMinutes
+const method10 = new Date();
+method10.getMinutes();  //  Return 0-59
+
+/*--------------------*/
+//  getSeconds
+const method11 = new Date();
+method11.getSeconds();  //  Returns 0-59
+
+/*--------------------*/
 
 
 
