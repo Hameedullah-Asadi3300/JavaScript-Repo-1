4680478db9d46a1ac68d1f35b3749e9f36b21e9b
Hameedullah Asadi <hameedasade20@gmail.com>
2023-07-19 19:06:21 -0700

    worked on modules in JavaScript

---
+++ Modules/main.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+import User, { firstFunction } from './modules.js';
+const user = new User("Hameed", 123456);
+console.log(user);
---
+++ Modules/modules.js
@@ -0,0 +1,30 @@
+/*
+modules introduced with ES6 in 2015. Module allows us to break our code into seperate files
+using "export" and "import" keywords.
+
+we have two types of export:
+1. named
+2. default
+
+We do export in two ways as folows:
+1. inline individually
+2. all at once at end
+*/
+
+
+
+class User {
+    constructor()
+    {
+        this.name = name
+        this.password = password
+        
+    }
+}
+
+//  function
+function firstFunction(user) {
+    console.log(`My username is ${uaer.name}`);
+}
+
+export { User, firstFunction }            //  The all at once export syntax
